0b24286ba81e3762b5224fdbf4540f8dee2dded1
Ryan Miller <64502062+RedLeafe@users.noreply.github.com>
2023-04-10 11:50:18 -0700

    Created the SettingsConfig Module file

---
+++ src/ReplicatedStorage/Configs/SettingsConfig.lua
@@ -0,0 +1,8 @@
+local module = {
+  {ID = "Popup_Effects", Description = "Disable the popup effects for lifting!", Enabled = true}
+  {ID = "Music", Description = "111111", Enabled = true}
+  {ID = "SFX", Description = "222222", Enabled = true}
+  {ID = "Trade_Request", Description = "333333", Enabled = true}
+}
+
+return module
